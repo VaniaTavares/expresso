fb68159ca52440ffb5817f3e9cba648a1328e1b2
Vania Tavares <vntavares56@gmail.com>
2022-03-29 12:37:44 +0100

    removed req variable

--- api/timesheetRouter.js
+++ api/timesheetRouter.js
@@ -1,5 +1,5 @@
 const timesheetRouter = require("express").Router({ mergeParams: true });
-const req = require("express/lib/request");
+
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
